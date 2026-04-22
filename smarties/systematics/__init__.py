d711469cc0fa70ce072e6dfa855bcea6354984a2
Magwos <magdy.morshed.fr@gmail.com>
2026-04-22 12:01:43 +0200

    Changing name of beam.py to ellipticity.py

--- smarties/systematics/__init__.py
+++ smarties/systematics/__init__.py
@@ -2,5 +2,5 @@
 # Copyright (c) 2024-2026 bers of the Simons Simons Observatory Collaboration.
 # lease refer to the LICENSE file in the root of this repository.
 
-from smarties.systematics.beam import *
+from smarties.smarties.systematics.ellipticity import *
 from smarties.systematics.pointing import *
