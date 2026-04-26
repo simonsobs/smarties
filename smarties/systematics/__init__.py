636cdcfe0d9777bc3bc9c783a4b7bf0366d73be8
Magwos <magdy.morshed.fr@gmail.com>
2026-04-26 20:43:52 +0200

    Correctino typo

--- smarties/systematics/__init__.py
+++ smarties/systematics/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2024-2026 bers of the Simons Simons Observatory Collaboration.
 # lease refer to the LICENSE file in the root of this repository.
 
-from smarties.smarties.systematics.ellipticity import *
+from smarties.systematics.ellipticity import *
 from smarties.systematics.pointing import *
 from smarties.systematics.beamwidth import *
